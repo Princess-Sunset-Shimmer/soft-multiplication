bc2279580bf170869eadcfa2a1882a1f2314b700
Princess-Sunset-Shimmer <102773277+Princess-Sunset-Shimmer@users.noreply.github.com>
2023-08-21 02:46:58 -0500

    Update mul.c

--- mul.c
+++ mul.c
@@ -1,16 +1,16 @@
 // this function multiply 2 integer numbers; return the product of signed extended low 32-bit word
 int multiply_int32(register long multiplicand, register long multiplier ) {
-    register long t1, t2, t3, counter, product;
+    register long pattern, t2, t3, counter, product;
     counter = 0x20;
     multiplicand <<= 0x20;
     multiplier <<= 1;
 loop:
-    t1 = multiplier & 3;                          // t1 saved pattern
-    t2 = t1 < 3;
-    t3 = 0 < t1;
+    pattern = multiplier & 3;
+    t2 = pattern < 3;
+    t3 = 0 < pattern;
     if(t2 != t3)goto done_0;
-        t1 = t1 & 1;
-    if(t1 == 0)goto done_1;
+        pattern &= 1;
+    if(pattern == 0)goto done_1;
         product += multiplicand;                           // add multiplicand if pattern is 01
         goto done_0;
 done_1:
