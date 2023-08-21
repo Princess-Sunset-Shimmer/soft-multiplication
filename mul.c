4e27225c990d3b13c689deed4136d39ddd3886b6
Princess-Sunset-Shimmer <102773277+Princess-Sunset-Shimmer@users.noreply.github.com>
2023-08-21 02:25:38 -0500

    Update mul.c

--- mul.c
+++ mul.c
@@ -1,5 +1,5 @@
 // this function multiply 2 integer numbers; return the product of signed extended low 32-bit word
-long mulw(register long multiplicand, register long multiplier ) {
+long multiply_half(register long multiplicand, register long multiplier ) {
     register long t1, t2, t3, t4, a0;       /* a0 is product */
     t4 = 0x20;                              /* t4 is counter */
     multiplicand <<= 0x20;
@@ -11,14 +11,14 @@ loop:
     if(t2 != t3)goto done_0;
     t1 = t1 & 0x1;
     if(t1 == 0)goto done_1;
-    a0 += multiplicand;                           // add multiplicand if pattern is 01
+    product += multiplicand;                           // add multiplicand if pattern is 01
     goto done_0;
 done_1:
-    a0 -= multiplicand;                           // sub multiplicand if pattern is 10
+    product -= multiplicand;                           // sub multiplicand if pattern is 10
 done_0:
     multiplier >>= 0x1;
-    a0 = a0 >> 0x1;
+    product >>= 0x1;
     t4 = t4 + (char signed)0xff;
     if(t4 != 0)goto loop;
-    return (int)a0;
+    return (int)product;
 }
