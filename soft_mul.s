160b097509513a95490a662fef55d6232886e97b
Princess-Sunset-Shimmer <102773277+Princess-Sunset-Shimmer@users.noreply.github.com>
2023-08-25 05:18:49 -0500

    Add files via upload

---
+++ soft_mul.s
@@ -0,0 +1,20 @@
+        .intel_syntax noprefix
+        .global multiply.half
+        .text
+multiply.half:  /* multiplicand, multiplier */
+        cmp     rax, 0
+        jz      multiply.half.done
+        xor     rcx, rcx                /* initiate previous_number, ZERO */
+        mov     rdx, rdi                /* cast next_number, multiplier */
+multiply.half.repeat:
+        mov     rsi, rax                /* cast argument_3, multiplicand */
+        and     rsi, 1                  /* and argument_2, LOWEST_BIT */
+        jz      multiply.half.zero
+        add     rcx, rdx                /* ahead previous_number, next_number */
+multiply.half.zero:
+        shl     rdx, 1
+        shr     rax, 1
+        jnz     multiply.half.repeat
+        mov     rax, rcx                /* cast return_value, half_product */
+multiply.half.done:
+        jmp     r11                     /* return */
